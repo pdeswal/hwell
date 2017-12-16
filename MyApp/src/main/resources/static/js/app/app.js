9ebab5cf3c85ba3b2538b7fd07838b01b59ada77
SAPIENT\kgopal <kgopalakrishna@sapient.com>
2017-12-16 14:09:38 +0530

    App JS changes

--- MyApp/src/main/resources/static/js/app/app.js
+++ MyApp/src/main/resources/static/js/app/app.js
@@ -2,7 +2,7 @@ var app = angular.module('crudApp',['ui.router','ngStorage']);
 
 app.constant('urls', {
     BASE: 'http://localhost:8080/MyApp',
-    USER_SERVICE_API : 'http://localhost:8080/MyApp/api/user/'
+    REPORT_SERVICE_API : 'http://localhost:8080/MyApp/api/report/'
 });
 
 app.config(['$stateProvider', '$urlRouterProvider',
@@ -11,14 +11,14 @@ app.config(['$stateProvider', '$urlRouterProvider',
         $stateProvider
             .state('home', {
                 url: '/',
-                templateUrl: 'partials/list',
-                controller:'UserController',
+                templateUrl: 'partials/report',
+                controller:'LayoutController',
                 controllerAs:'ctrl',
                 resolve: {
-                    users: function ($q, UserService) {
-                        console.log('Load all users');
+                    reports: function ($q, ReportService) {
+                        console.log('Load all report');
                         var deferred = $q.defer();
-                        UserService.loadAllUsers().then(deferred.resolve, deferred.resolve);
+                        ReportService.getAllppliancesData().then(deferred.resolve, deferred.resolve);
                         return deferred.promise;
                     }
                 }
