cd7c6fdb15f8b31ac865a34034a5b5390960782d
Abhirag Kesarwani <AKesarwani@manh.com>
2017-12-16 14:07:48 +0530

    Sensor

--- MyApp/src/main/java/com/pd/apps/model/Sensors.java
+++ MyApp/src/main/java/com/pd/apps/model/Sensors.java
@@ -5,122 +5,113 @@ import org.hibernate.validator.constraints.NotEmpty;
 import javax.persistence.*;
 import java.io.Serializable;
 
-@SuppressWarnings("serial")
+
+
 @Entity
 @Table(name="Sensors")
 public class Sensors implements Serializable{
 
-@Id
-@GeneratedValue(strategy= GenerationType.IDENTITY)
-private int id;
+/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
-@NotEmpty
-@Column(name="S_ID", nullable=false)
-private String s_id;
+	@Id
+	@GeneratedValue(strategy= GenerationType.IDENTITY)
+	private Integer id;
 
-@Column(name="S_Type", nullable=false)
-private Integer s_Type;
+	@NotEmpty
+	@Column(name="S_ID", nullable=false)
+	private String sId;
 
-@Column(name="Layout_ID", nullable=false)
-private double layout_ID;
+	@Column(name="S_Type", nullable=false)
+	private String sType;
 
-public int getId() {
-return id;
-}
+	@Column(name="Layout_ID", nullable=false)
+	private String layoutId;
 
+	public Integer getId() {
+		return id;
+	}
 
+	public void setId(Integer id) {
+		this.id = id;
+	}
 
-public String getS_id() {
-return s_id;
-}
+	public String getsId() {
+		return sId;
+	}
 
+	public void setsId(String sId) {
+		this.sId = sId;
+	}
 
+	public String getsType() {
+		return sType;
+	}
 
-public void setS_id(String s_id) {
-this.s_id = s_id;
-}
+	public void setsType(String sType) {
+		this.sType = sType;
+	}
 
+	public String getLayoutid() {
+		return layoutId;
+	}
 
+	public void setLayoutid(String layoutid) {
+		this.layoutId = layoutid;
+	}
 
-public Integer getS_Type() {
-return s_Type;
-}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((layoutId == null) ? 0 : layoutId.hashCode());
+		result = prime * result + ((sId == null) ? 0 : sId.hashCode());
+		result = prime * result + ((sType == null) ? 0 : sType.hashCode());
+		return result;
+	}
 
-
-
-public void setS_Type(Integer s_Type) {
-this.s_Type = s_Type;
-}
-
-
-
-public double getLayout_ID() {
-return layout_ID;
-}
-
-
-
-public void setLayout_ID(double layout_ID) {
-this.layout_ID = layout_ID;
-}
-
-
-
-public void setId(int id) {
-this.id = id;
-}
-
-
-
-@Override
-public int hashCode() {
-	final int prime = 31;
-	int result = 1;
-	result = prime * result + id;
-	long temp;
-	temp = Double.doubleToLongBits(layout_ID);
-	result = prime * result + (int) (temp ^ (temp >>> 32));
-	result = prime * result + ((s_Type == null) ? 0 : s_Type.hashCode());
-	result = prime * result + ((s_id == null) ? 0 : s_id.hashCode());
-	return result;
-}
-
-
-
-@Override
-public boolean equals(Object obj) {
-	if (this == obj)
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sensors other = (Sensors) obj;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (layoutId == null) {
+			if (other.layoutId != null)
+				return false;
+		} else if (!layoutId.equals(other.layoutId))
+			return false;
+		if (sId == null) {
+			if (other.sId != null)
+				return false;
+		} else if (!sId.equals(other.sId))
+			return false;
+		if (sType == null) {
+			if (other.sType != null)
+				return false;
+		} else if (!sType.equals(other.sType))
+			return false;
 		return true;
-	if (obj == null)
-		return false;
-	if (getClass() != obj.getClass())
-		return false;
-	Sensors other = (Sensors) obj;
-	if (id != other.id)
-		return false;
-	if (Double.doubleToLongBits(layout_ID) != Double.doubleToLongBits(other.layout_ID))
-		return false;
-	if (s_Type == null) {
-		if (other.s_Type != null)
-			return false;
-	} else if (!s_Type.equals(other.s_Type))
-		return false;
-	if (s_id == null) {
-		if (other.s_id != null)
-			return false;
-	} else if (!s_id.equals(other.s_id))
-		return false;
-	return true;
-}
+	}
 
+	@Override
+	public String toString() {
+		return "Sensors [id=" + id + ", sId=" + sId + ", sType=" + sType + ", layoutId=" + layoutId + "]";
+	}
 
-
-@Override
-public String toString() {
-	return "Sensors [id=" + id + ", s_id=" + s_id + ", s_Type=" + s_Type + ", layout_ID=" + layout_ID + "]";
-}
-
-
+	
+	
 
 
 }
