e1930a38da5ebf1a7528b9ac90cc23bb0a28e7c9
Abhirag Kesarwani <AKesarwani@manh.com>
2017-12-16 12:22:48 +0530

    Building Layout

---
+++ MyApp/src/main/java/com/pd/apps/model/Building.java
@@ -0,0 +1,96 @@
+package com.pd.apps.model;
+
+import org.hibernate.validator.constraints.NotEmpty;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(name="Layout")
+public class Building implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	private String id;
+
+	@NotEmpty
+	@Column(name="NAME", nullable=false)
+	private String name;
+
+	@Column(name="P_Name")
+	private String P_Name;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getP_Name() {
+		return P_Name;
+	}
+
+	public void setP_Name(String p_Name) {
+		P_Name = p_Name;
+	}
+
+	@Override
+	public String toString() {
+		return "Building [id=" + id + ", name=" + name + ", P_Name=" + P_Name + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((P_Name == null) ? 0 : P_Name.hashCode());
+		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Building other = (Building) obj;
+		if (P_Name == null) {
+			if (other.P_Name != null)
+				return false;
+		} else if (!P_Name.equals(other.P_Name))
+			return false;
+		if (id == null) {
+			if (other.id != null)
+				return false;
+		} else if (!id.equals(other.id))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+
+	
+
+}
